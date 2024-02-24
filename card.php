674b824ed9776d4d8250a1f0b551b1a753986761
MrushikPatel <160627189+MrushikPatel@users.noreply.github.com>
2024-02-24 21:43:46 +0530

    NEW FILE

---
+++ card.php
@@ -0,0 +1,169 @@
+<html>
+
+    <head>
+        <link rel="stylesheet" href="card.css">
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet"
+            integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css"
+            integrity="sha512-DTOQO9RWCH3ppGqcWaEA1BIZOC6xxalwEsw9c2QQeAIftl+Vegovlnee1c9QX4TctnWMn13TZye+giMm8e2LwA=="
+            crossorigin="anonymous" referrerpolicy="no-referrer" />
+
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css"
+            integrity="sha512-DTOQO9RWCH3ppGqcWaEA1BIZOC6xxalwEsw9c2QQeAIftl+Vegovlnee1c9QX4TctnWMn13TZye+giMm8e2LwA=="
+            crossorigin="anonymous" referrerpolicy="no-referrer" />
+
+        <link rel="icon" href="image/Navy Blue Urban Modern Real Estate Logo (1).png" class="img-fluid">
+    </head>
+
+    <body>
+    <?php include 'hfile.php'; ?>
+    <spam>
+        <div class="container d-flex Mcard mt-5">
+            <div class="card" style="width: 18rem;">
+                <img class="card-img-top" src="IMAGES/12.png" alt="Card image cap">
+                <div class="card-body">
+                    <h5 class="card-title">Name</h5>
+                    <div class="loc">
+                        <p>Loaction</p>
+                    </div>
+                    <div class="pr">
+                        <p>Price</p>
+                    </div>
+                    <div class="bhk">
+                        <p>BHK</p>
+                    </div>
+                    <button>View More</button>
+                </div>
+            </div>
+
+            <div class="card" style="width: 18rem;">
+                <img class="card-img-top" src="IMAGES/12.png" alt="Card image cap">
+                <div class="card-body">
+                    <h5 class="card-title">Name</h5>
+                  <div class="loc">
+                        <p>Loaction</p>
+                    </div>
+                    <div class="pr">
+                        <p>Price</p>
+                    </div>
+                    <div class="bhk">
+                        <p>BHK</p>
+                    </div>
+                    <button>View More</button>
+                </div>
+            </div>
+
+
+            <div class="card" style="width: 18rem;">
+                <img class="card-img-top" src="IMAGES/12.png" alt="Card image cap">
+                <div class="card-body">
+                    <h5 class="card-title">Name</h5>
+                  <div class="loc">
+                        <p>Loaction</p>
+                    </div>
+                    <div class="pr">
+                        <p>Price</p>
+                    </div>
+                    <div class="bhk">
+                        <p>BHK</p>
+                    </div>
+                    <button>View More</button>
+                </div>
+            </div>
+
+
+            <div class="card" style="width: 18rem;">
+                <img class="card-img-top" src="IMAGES/12.png" alt="Card image cap">
+                <div class="card-body">
+                    <h5 class="card-title">Name</h5>
+                  <div class="loc">
+                        <p>Loaction</p>
+                    </div>
+                    <div class="pr">
+                        <p>Price</p>
+                    </div>
+                    <div class="bhk">
+                        <p>BHK</p>
+                    </div>
+                    <button>View More</button>
+                </div>
+            </div>
+
+        </div>
+
+
+        <div class="container d-flex Mcard mt-5">
+            <div class="card" style="width: 18rem;">
+                <img class="card-img-top" src="IMAGES/12.png" alt="Card image cap">
+                <div class="card-body">
+                    <h5 class="card-title">Name</h5>
+                    <div class="loc">
+                        <p>Loaction</p>
+                    </div>
+                    <div class="pr">
+                        <p>Price</p>
+                    </div>
+                    <div class="bhk">
+                        <p>BHK</p>
+                    </div>
+                    <button>View More</button>
+                </div>
+            </div>
+
+            <div class="card" style="width: 18rem;">
+                <img class="card-img-top" src="IMAGES/12.png" alt="Card image cap">
+                <div class="card-body">
+                    <h5 class="card-title">Name</h5>
+                  <div class="loc">
+                        <p>Loaction</p>
+                    </div>
+                    <div class="pr">
+                        <p>Price</p>
+                    </div>
+                    <div class="bhk">
+                        <p>BHK</p>
+                    </div>
+                    <button>View More</button>
+                </div>
+            </div>
+
+
+            <div class="card" style="width: 18rem;">
+                <img class="card-img-top" src="IMAGES/12.png" alt="Card image cap">
+                <div class="card-body">
+                    <h5 class="card-title">Name</h5>
+                  <div class="loc">
+                        <p>Loaction</p>
+                    </div>
+                    <div class="pr">
+                        <p>Price</p>
+                    </div>
+                    <div class="bhk">
+                        <p>BHK</p>
+                    </div>
+                    <button>View More</button>
+                </div>
+            </div>
+
+
+            <div class="card" style="width: 18rem;">
+                <img class="card-img-top" src="IMAGES/12.png" alt="Card image cap">
+                <div class="card-body">
+                    <h5 class="card-title">Name</h5>
+                  <div class="loc">
+                        <p>Loaction</p>
+                    </div>
+                    <div class="pr">
+                        <p>Price</p>
+                    </div>
+                    <div class="bhk">
+                        <p>BHK</p>
+                    </div>
+                    <button>View More</button>
+                </div>
+            </div>
+
+        </div>
+    </body>
+
+</html>
